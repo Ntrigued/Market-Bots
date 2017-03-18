ef8c96aad68f8cc9a7a2a2f6d170d491893bdfb7
Ntrigued <virtfund@gmail.com>
2017-03-18 18:02:03 -0500

    General Update #1

--- Agents/BasicAgent.java
+++ Agents/BasicAgent.java
@@ -2,11 +2,13 @@ package Agents;
 
 import Markets.Order;
 import Products.*;
+import Testing.*;
 
 import java.util.*;
 
 public class BasicAgent extends AbstractAgent{
 	
+	Output test = new Output();
 	int orderID;
 	Map<String, Integer> productInventory;
 	
@@ -15,9 +17,20 @@ public class BasicAgent extends AbstractAgent{
 		setBalance(balance);
 		productInventory = new HashMap<String, Integer>();
 		
-		for(String product : products) productInventory.put(product, 0);
+		for(String product : products) productInventory.put(product, 100);
 		
 		orderID = 0;
+	}
+	
+	public void closingActions()
+	{
+		test.println("Apples: " + productInventory.get("Apple"));
+		test.println("balance: " + getBalance());
+	}
+	
+	public int getItemInventory(String productName)
+	{
+		return productInventory.get(productName);
 	}
 	
 	//Called during agent interaction stage
@@ -26,25 +39,51 @@ public class BasicAgent extends AbstractAgent{
 	//Called if order has been filled
 	public void receiveInventoryChange() {} 
 	
+	int x = 0;
 	//Called for bid/offer stage
 	public Order[] sendOrderDetails()
 	{
-		orderID++;
-		int orderNum = (int) Math.ceil(.25 * (getBalance()/5));
-		Order order = new Order(new Apple(), orderNum, 5.0, 1, orderID);
+		int orderType;
+		int orderNum;
+		
+		if(getBalance() > 1000) 
+		{
+			orderID++;
+			orderType = 1;
+			orderNum = (int) Math.rint(.25 * (getBalance()));
+			
+		} else if(productInventory.get("Apple") > 0)
+		{
+			orderNum = productInventory.get("Apple");
+			orderType = 0;
+			
+		} else return null;
+		
+		Order order = new Order(new Apple(), orderNum, 5.0, orderType, orderID);
 		Order[] orders = new Order[1];
 		orders[0] = order;
-		
+
 		return orders;
 	} 
 	
-	public void receiveInventoryChange(String productName, int amountTraded, double balanceChange)
+	public boolean receiveInventoryChange(String productName, int amountTraded, double balanceChange)
 	{
+		System.out.println("recieveInventoryChange: " + productName + "  " + amountTraded + "  " + balanceChange);
 		int currentQuantity = productInventory.get(productName);
-		productInventory.put(productName, currentQuantity + amountTraded);
+		if(currentQuantity + amountTraded < 0) return false;
 		
 		double currentBalance = getBalance();
+		if(currentBalance + balanceChange < 0) return false;
+		
+		productInventory.put(productName, currentQuantity + amountTraded);
 		setBalance(currentBalance + balanceChange);
+		
+		return true;
+	}
+
+	public void setItemInventory(String productName, int amount)
+	{
+		productInventory.put(productName, amount);
 	}
 	
 	//called for update accounting stage
@@ -54,5 +93,4 @@ public class BasicAgent extends AbstractAgent{
 	{
 		
 	}
-	
 }

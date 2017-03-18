8b6c9201ef151cfd0280bec4f3b7337da467c8e6
Ntrigued <virtfund@gmail.com>
2017-03-18 18:10:06 -0500

    General Update #1

--- Driver.java
+++ Driver.java
@@ -1,22 +1,25 @@
 import Agents.*;
+import Communication.Message;
 import Markets.*;
 import Products.*;
 import Testing.*;
 
-/* Sample, extremely simple market */
+
 public class Driver {
 
 	public static void main(String[] args)
 	{
-		Output test = new Output();
+		Output test = new Output(); //Testing class
 		String[] productList = ProductList.getList();
 		
-		BasicAgent[] agents = 
-		{
-				new BasicAgent(100000000, productList),
-				new BasicAgent(337, productList)
+		AbstractAgent[] agents = 
+		{ //Place market agents in here
+				new BasicUtilityAgent(10000, productList),
+				new AltUtilityAgent(10000, productList),
+				new BasicAgent(200, productList)
 		};
 		
-		BasicMarket market = new BasicMarket(agents, productList, 10);
+		//Choose the type of market that the agents will interact in
+		BasicCommunicatingMarket market = new BasicCommunicatingMarket(agents, productList, 10); //Runs market loop automatically
 	}
 }

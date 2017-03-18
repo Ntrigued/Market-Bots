0f2096a5ccc2dc75ae9e9b9f65c65017d895dfee
Ntrigued <virtfund@gmail.com>
2017-03-18 17:17:57 -0500

    General Update #1

--- Agents/AbstractAgent.java
+++ Agents/AbstractAgent.java
@@ -8,9 +8,10 @@ public abstract class AbstractAgent {
 	
 	public void closingActions() {} //called after market loop has completed
 	public double getBalance() { return this.balance; }
+	public void setCurrentPrice(String productName, double price) {}
 	public void interact() {} //Called during agent interaction stage
 	public void otherActions() {} //last method called in marketloop
-	public void receiveInventoryChange(String productName, int amountTraded, double balanceChange) {} //Called if order has been filled
+	public boolean receiveInventoryChange(String productName, int amountTraded, double balanceChange) { return true; } //Called if order has been filled
 	public Order[] sendOrderDetails() { return new Order[1]; } //Called for bid/offer stage
 	public void setBalance(double balance) { this. balance = balance; }
 	public void updateAccounting() {} //called for update accounting stage

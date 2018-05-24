821d15fdb9c87760f533205d4638f7d4fb377dc9
Ntrigued <virtfund@gmail.com>
2018-05-24 13:40:51 -0500

    Update AbstractAgent.java

--- Agents/AbstractAgent.java
+++ Agents/AbstractAgent.java
@@ -1,18 +1,19 @@
-package Agents;
+package MarketBots.Agents;
 
-import Markets.Order;
+import MarketBots.Markets.Order;
 
-public abstract class AbstractAgent {
+public abstract class AbstractAgent implements AbstractAgentInterface{
 	
 	private double balance;
 	
 	public void closingActions() {} //called after market loop has completed
 	public double getBalance() { return this.balance; }
-	public void setCurrentPrice(String productName, double price) {}
+	public void setCurrentTrade(String productName, double price, int tradeAmount) {}
 	public void interact() {} //Called during agent interaction stage
 	public void otherActions() {} //last method called in marketloop
+	public void receive(String msg) {} // For sending free-form message to an agent
 	public boolean receiveInventoryChange(String productName, int amountTraded, double balanceChange) { return true; } //Called if order has been filled
-	public Order[] sendOrderDetails() { return new Order[1]; } //Called for bid/offer stage
+	public Order[] sendOrderDetails() { return new Order[0]; } //Called for bid/offer stage
 	public void setBalance(double balance) { this. balance = balance; }
 	public void updateAccounting() {} //called for update accounting stage
 }

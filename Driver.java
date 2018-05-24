4f0a9a6d221b8d867ad9f2c5a13ea1f2b7d9b3ff
Ntrigued <virtfund@gmail.com>
2018-05-24 13:50:22 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,25 +1,89 @@
-import Agents.*;
-import Communication.Message;
-import Markets.*;
-import Products.*;
-import Testing.*;
+package MarketBots;
 
+import java.util.*;
+
+import MarketBots.Agents.*;
+import MarketBots.Agents.Implementations.AltUtilInternetAgent;
+import MarketBots.Agents.Implementations.UtilInternetAgent;
+import MarketBots.Markets.*;
+import MarketBots.Products.*;
+
+import MarketBots.BondMarketTest.*;
 
 public class Driver {
 
 	public static void main(String[] args)
 	{
-		Output test = new Output(); //Testing class
-		String[] productList = ProductList.getList();
+		///////////////////////////////////////////////////////////////////////
+		//                                                                   //
+		// Products (defined in MarketBots.Products) used in this simulation //
+		// 		Used in argument for agent classes                           //
+		//                                                                   //
+		///////////////////////////////////////////////////////////////////////
+		String[] productList = new String[]
+				{
+					"BasicBond"
+				};
 		
-		AbstractAgent[] agents = 
-		{ //Place market agents in here
-				new BasicUtilityAgent(10000, productList),
-				new AltUtilityAgent(10000, productList),
-				new BasicAgent(200, productList)
-		};
+		//////////////////////////////////////////////////////////
+		//                                                      //
+		// Place all agents in this array                       //
+		// Except the Internet Provider                         //
+		//                                                      //
+		//////////////////////////////////////////////////////////
+		AbstractAgentInterface[] agentArray = new AbstractAgentInterface[]
+				{
+					new InfoGatherAgent(productList), //Gathers Trade history data
+					new SellerAgent(10000.0),
+					new SellerAgent(10000.0)
+				};
 		
-		//Choose the type of market that the agents will interact in
-		BasicCommunicatingMarket market = new BasicCommunicatingMarket(agents, productList, 10); //Runs market loop automatically
+		ArrayList<AbstractAgentInterface> nonInternetAgents = new ArrayList<AbstractAgentInterface>();
+		for(AbstractAgentInterface agent : agentArray)
+		{
+			nonInternetAgents.add(agent);
+		}
+		
+		ArrayList<InternetAgentInterface> internetAgentsList = new ArrayList<InternetAgentInterface>();
+		for(AbstractAgentInterface agent : nonInternetAgents)
+			if(agent instanceof InternetAgentInterface)
+				internetAgentsList.add((InternetAgentInterface) agent);
+		InternetAgentInterface[] internetAgents = new InternetAgentInterface[internetAgentsList.size()];
+		for(int i = 0; i < internetAgentsList.size(); i++)
+			internetAgents[i] = internetAgentsList.get(i);
+		
+		//////////////////////////////////////////////////////////////////////////////////
+		//                                                                              //
+		// Place Internet Provider here                                                 //
+		//	- with internetAgents as all agents which  implement InternetAgentInterface //
+		//                                                                              //
+		//////////////////////////////////////////////////////////////////////////////////
+		InternetProviderInterface internetProvider = new BasicInternetProviderAgent(internetAgents);
+		
+		AbstractAgentInterface[] agents = new AbstractAgentInterface[1 + nonInternetAgents.size()];
+		for(int i = 0; i < agents.length - 1; i++)
+		{
+			agents[i] = nonInternetAgents.get(i);
+		}
+		agents[agents.length - 1] = internetProvider;
+				
+		//////////////////////////////////////////////////////////////////////
+		//                                                                  //                  
+		// Choose the type of market that the agents will interact in       //
+		//                                                                  //
+		//////////////////////////////////////////////////////////////////////
+		BasicCommunicatingMarket market = new BasicCommunicatingMarket(agents, productList);
+		
+		///////////////////////////////////////////
+		//                                       //
+		// run time steps here                   //
+		// and perform inter-time step actions   //
+		//                                       //
+		///////////////////////////////////////////
+		market.runTimeSteps(500);
+	 // market.addAgent( new BasicAgent(1000, productList) );
+	 // market.runTimeSteps(500);
+		
+		market.close();
 	}
 }

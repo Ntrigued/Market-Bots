1fbb98a0d6999e9fd3c1a1949f785e2b0cdd9bdc
Matthew Allbee <allbeematthew@gmail.com>
2019-02-07 15:02:45 -0600

    Revert "Revert "Maven Integration Completed""
    This reverts commit 0549677f1a3122a930b0dc0b57c579a5ca8b7a64.

---
+++ src/Main/Java/Agents/Implementations/BasicInternetAgent.java
@@ -0,0 +1,39 @@
+package Agents.Implementations;
+
+import Agents.BasicAgent;
+import Agents.InternetAgentInterface;
+import Agents.InternetProviderInterface;
+import Communication.*;
+
+public class BasicInternetAgent extends BasicAgent implements InternetAgentInterface
+{
+	private int ID;
+	private InternetProviderInterface internet;
+	
+	public BasicInternetAgent(double balance, String[] products)
+	{
+		super(balance, products);
+	}
+	
+	public int getID()
+	{
+		return this.ID;
+	}
+	
+	public void receiveID(int ID)
+	{
+		this.ID = ID;
+	}
+	
+	public Message receiveMsg(Message msg) 
+	{
+		return new Message(this.ID, msg.getFromID(), 
+				  		   "EXAMPLE_RESPONSE", "thanks for the message");
+	}
+	
+	public Message sendMsg() {return null;}
+
+	public void setInternetProvider(InternetProviderInterface internet) {
+		this.internet = internet;
+	}
+}

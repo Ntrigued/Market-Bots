968f2c04fb7c00f5e9a4a73db6b0cf51ffb46800
Matthew Allbee <allbeematthew@gmail.com>
2019-02-07 14:47:35 -0600

    Maven Integration Completed

---
+++ src/Main/Java/Agents/Implementations/AltUtilInternetAgent.java
@@ -0,0 +1,55 @@
+package Agents.Implementations;
+
+import Agents.InternetAgentInterface;
+import Agents.InternetProviderInterface;
+import Communication.Message;
+
+public class AltUtilInternetAgent extends AltUtilityAgent implements InternetAgentInterface
+{
+	private int ID;
+	private InternetProviderInterface internet;
+	private Integer[] agentIDs;
+	
+	public AltUtilInternetAgent(double balance, String[] products)
+	{
+		super(balance, products);
+		this.ID = 0;
+		this.internet = null;
+	}
+	
+	public int getID()
+	{
+		return this.ID;
+	}
+
+	public void receiveID(int ID)
+	{
+		this.ID = ID;
+	}
+	
+	public Message receiveMsg(Message msg) 
+	{
+		System.out.println(msg.getMessage());
+		return null;
+	}
+	
+	public Message sendMsg() 
+	{
+		for(int agentID : agentIDs)
+		{
+			InternetAgentInterface agent = this.internet.getAgentByID(agentID);
+			if(agent != null && agent instanceof UtilInternetAgent)
+			{
+				System.out.println(agent.getClass());
+				return new Message(this.ID, agent.getID(), "EXAMPLE_MESSAGE", "here's your message");
+			}
+		}
+		
+		return null;
+	}
+
+	public void setInternetProvider(InternetProviderInterface internet) {
+		this.internet = internet;
+		this.agentIDs = this.internet.getAgentIDs();
+	}
+}
